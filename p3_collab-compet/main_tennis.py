7dde312458ab1b38d2d4167b8b97cff70d95a74d
Wei Zhao <zhaoweiphy@gmail.com>
2020-07-06 00:32:24 -0500

    feat: Fix bugs to make main_tennis work

--- p3_collab-compet/main_tennis.py
+++ p3_collab-compet/main_tennis.py
@@ -36,8 +36,8 @@ seeding()
 parallel_envs = 1
 # number of training episodes.
 # change this to higher number to experiment. say 30000.
-number_of_episodes = 3000
-batchsize = 1000
+number_of_episodes = 300
+batchsize = 10
 # how many episodes to save policy and gif
 save_interval = 1000
 t = 0
@@ -62,43 +62,75 @@ logger = SummaryWriter(log_dir=log_path)
 agent0_reward = []
 agent1_reward = []
 
-while True:
+reward_this_episode = np.zeros((parallel_envs, num_agents))
+env_info = env.reset(train_mode=True)[brain_name] 
+all_obs = env_info.vector_observations
+
+obs = [list(all_obs)]
+obs_full= [np.concatenate(all_obs)]
+
+for episode in range(0, number_of_episodes):
+
+    while True:
+        # explore = only explore for a certain number of episodes
+        # action input needs to be transposed
+        actions = maddpg.act(transpose_to_tensor(obs), noise=noise)
+        noise *= noise_reduction
+
+        actions_array = torch.stack(actions).detach().numpy()
+
+        # transpose the list of list
+        # flip the first two indices
+        # input to step requires the first index to correspond to number of parallel agents
+        actions_for_env = np.rollaxis(actions_array,1)
+
+        # step forward
+        env_info = env.step(actions_for_env[0])[brain_name]
+        next_obs_all = env_info.vector_observations         # get next state (for each agent)
+        next_obs = [list(next_obs_all)]
+        next_obs_full= [np.concatenate(next_obs_all)]
+        rewards  = [env_info.rewards]                         # get reward (for each agent)
+        dones    = [env_info.local_done]                        # see if episode finished
 
 
-    # explore = only explore for a certain number of episodes
-    # action input needs to be transposed
-    actions = maddpg.act(transpose_to_tensor(obs), noise=noise)
-    noise *= noise_reduction
-    
-    actions_array = torch.stack(actions).detach().numpy()
+        # add data to buffer
+        transition = (obs, obs_full, actions_for_env, rewards, next_obs, next_obs_full, dones)
 
-    # transpose the list of list
-    # flip the first two indices
-    # input to step requires the first index to correspond to number of parallel agents
-    actions_for_env = np.rollaxis(actions_array,1)
-    
-    # step forward
-    env_info = env.step(actions_for_env[0])[brain_name]
-    next_obs_all = env_info.vector_observations         # get next state (for each agent)
-    next_obs = [list(next_obs_all)]
-    next_obs_full= [np.concatenate(next_obs_all)]
-    rewards  = env_info.rewards                         # get reward (for each agent)
-    dones    = env_info.local_done                        # see if episode finished
-    
-           
-    # add data to buffer
-    transition = (obs, obs_full, actions_for_env, rewards, next_obs, next_obs_full, dones)
-    
-    buffer.push(transition)
-    
-    reward_this_episode += rewards
+        buffer.push(transition)
 
-    obs, obs_full = next_obs, next_obs_full
-    if np.any(dones):
-        break
-        
-samples = buffer.sample(10)
-maddpg.update(samples, 1, logger)
+        reward_this_episode += rewards
 
+        obs, obs_full = next_obs, next_obs_full
+        if np.any(dones):
+            break
 
+    # if len(buffer) > batchsize and episode % episode_per_update < parallel_envs:
+    if len(buffer) > batchsize:
+        for a_i in range(num_agents):
+            samples = buffer.sample(batchsize)
+            maddpg.update(samples, a_i, logger)
+        maddpg.update_targets() #soft update the target network towards the actual networks
 
+    for i in range(parallel_envs):
+        agent0_reward.append(reward_this_episode[i,0])
+        agent1_reward.append(reward_this_episode[i,1])
+
+    if episode % 100 == 0 or episode == number_of_episodes-1:
+        avg_rewards = [np.mean(agent0_reward), np.mean(agent1_reward)]
+        agent0_reward = []
+        agent1_reward = []
+        for a_i, avg_rew in enumerate(avg_rewards):
+            logger.add_scalar('agent%i/mean_episode_rewards' % a_i, avg_rew, episode)
+
+    save_dict_list =[]
+    if episode % 100 == 0:
+        for i in range(num_agents):
+            save_dict = {'actor_params' : maddpg.maddpg_agent[i].actor.state_dict(),
+                         'actor_optim_params': maddpg.maddpg_agent[i].actor_optimizer.state_dict(),
+                         'critic_params' : maddpg.maddpg_agent[i].critic.state_dict(),
+                         'critic_optim_params' : maddpg.maddpg_agent[i].critic_optimizer.state_dict()}
+            save_dict_list.append(save_dict)
+            torch.save(save_dict_list, os.path.join(model_dir, 'episode-{}.pt'.format(episode)))
+
+env.close()
+logger.close()
